d61c1ef0e71268348b4668cd4362fa93eed091a0
datpt <phungdat14102003@gmail.com>
2025-03-05 00:33:13 +0700

    add user with image picker and fragment

---
+++ app/src/main/java/com/example/demoroomdatabase/fragment/ViewPagerAdapter.java
@@ -0,0 +1,30 @@
+package com.example.demoroomdatabase.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0: return new HomeFragment();
+            case 1: return new FavouriteFragment();
+            case 2: return new ProfileFragment();
+            default: return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3; // Số lượng Fragment
+    }
+}
