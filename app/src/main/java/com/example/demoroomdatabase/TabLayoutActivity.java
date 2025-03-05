353d5c3d8b312456060f2e3811081a8023cad63b
datpt <phungdat14102003@gmail.com>
2025-03-05 12:55:31 +0700

    update TabLayout

--- app/src/main/java/com/example/demoroomdatabase/TabLayoutActivity.java
+++ app/src/main/java/com/example/demoroomdatabase/TabLayoutActivity.java
@@ -1,41 +1,62 @@
 package com.example.demoroomdatabase;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.demoroomdatabase.fragment.TabLayoutAdapter;
 import com.example.demoroomdatabase.fragment.ViewPagerAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
-public class ViewPager extends AppCompatActivity {
+public class TabLayoutActivity extends AppCompatActivity {
+
     BottomNavigationView bottomNavigationView;
+    TabLayout Tablayout;
     ViewPager2 viewPager;
-    ViewPagerAdapter viewPagerAdapter;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_view_pager);
+        setContentView(R.layout.activity_tab_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        bottomNavigationView = findViewById(R.id.bottom_nav);
+        Tablayout = findViewById(R.id.tab_layout);
         viewPager = findViewById(R.id.view_pager);
-        viewPagerAdapter = new ViewPagerAdapter(this);
-        viewPager.setAdapter(viewPagerAdapter);
+        ViewPagerAdapter viewPagerAdapter= new ViewPagerAdapter(this);
+
+
+        TabLayoutAdapter tabLayoutAdapter = new TabLayoutAdapter(this);
+        viewPager.setAdapter(tabLayoutAdapter);
+
+        // Đồng bộ hóa TabLayout với ViewPager2
+        new TabLayoutMediator(Tablayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                if (position == 0) {
+                    tab.setText("Home");
+                } else if (position == 1) {
+                    tab.setText("Favourite");
+                }
+            }
+        }).attach();
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnItemSelectedListener(item -> {
-            Fragment selectedFragment = null;
+            viewPager.setAdapter(viewPagerAdapter);
             if (item.getItemId() == R.id.action_home) {
                 viewPager.setCurrentItem(0);
                 //selectedFragment = new HomeFragment();
--- app/src/main/java/com/example/demoroomdatabase/fragment/TabLayoutAdapter.java
+++ app/src/main/java/com/example/demoroomdatabase/fragment/TabLayoutAdapter.java
@@ -3,12 +3,10 @@ package com.example.demoroomdatabase.fragment;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-public class ViewPagerAdapter extends FragmentStateAdapter {
-    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+public class TabLayoutAdapter extends FragmentStateAdapter {
+    public TabLayoutAdapter(@NonNull FragmentActivity fragmentActivity) {
         super(fragmentActivity);
     }
 
@@ -18,14 +16,13 @@ public class ViewPagerAdapter extends FragmentStateAdapter {
         switch (position) {
             case 0: return new HomeFragment();
             case 1: return new FavouriteFragment();
-            case 2: return new ProfileFragment();
             default: return new HomeFragment();
         }
     }
 
     @Override
     public int getItemCount() {
-        return 3; // Số lượng Fragment
+        return 2; // Số lượng Fragment
     }
 
 }
